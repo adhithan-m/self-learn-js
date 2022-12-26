d6a7acbdc68ffb5edabb9405dc7c754a2617ddf5
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 22:27:39 +0530

    self learn for js

---
+++ self-learn/basic js/adhi.js
@@ -0,0 +1,16 @@
+function adhi(){
+var a = {"NAME":"Adhithan.M",
+         "AGE":22,
+		 "MARTIAl.STATUS":"Unmarried",
+         "LANGUAGE":"Tamil,English,Hindi"
+		 }
+		 {
+           console.log(a);
+		 }
+}		 
+		 
+
+//for (key in a)
+//{
+	//console.log(a[])
+//}
---
+++ self-learn/js home/hello.js
@@ -0,0 +1,35 @@
+function wish()
+{
+   alert("hello! good morning");
+}
+function tif()
+{
+   alert("Have a tiffon?");
+}
+
+var a = prompt("Enter the value","The value is");
+var b = parseInt(prompt("Enter the number"));
+var c =null;
+{
+	alert("This is a"+" "+typeof(a)+" "+"and its value is"+" "+ a);
+} 
+{
+	alert("This is a"+" "+typeof(b)+" "+"and its value is"+" "+ b);
+} 
+{
+	alert("This is a"+" "+typeof(c)+" "+"and its value is"+" "+ c);
+} 
+
+
+var a = prompt("Enter the value","The value is");
+{
+	alert("This is a"+" "+typeof(a)+" "+"and its value is"+" "+ a);
+}
+if(confirm("Do you want to quit")==true)
+{
+	alert("Thank you");
+}
+else
+{
+	alert("proceed");
+}
